1ab5ce53007d8c70ff9e90217d804a501981e26d
Nina Stoyanova <nina.stoqnova91@gmail.com>
2021-02-10 19:06:24 +0200

    layout

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+
+// var array = ["nina", "pepi", 8];
+
+
+// // for (var i=0; i<array.length; i++) {  //0 1 2
+// //    var element = array[i];  
+// //     console.log(element);      //print all element of the array
+// // }
+
+
+// for (var element of array) { //var elemet is var i=0...)
+//   console.log(element);  //print all elements of the array
+// }
+
+
+// for (var i=0; i<10; i=i+2) { //0123456789 //02468
+//    console.log(i);  //print 0-1000
+// }
+
+
+
